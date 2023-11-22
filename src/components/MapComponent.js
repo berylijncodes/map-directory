f58e34c97fed63e2923c7b7362d7764e3f4f7798
Beryl Ilenwabor <berylijn@gmail.com>
2023-11-22 16:13:14 +0100

    Integrate google map to app

---
+++ src/components/MapComponent.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+
+const MapComponent = ({ selectedLocation }) => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+  });
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+  if (loadError) return "Error";
+  if (!isLoaded) return "Maps";
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={{
+          height: "630px",
+        }}
+        center={selectedLocation}
+        zoom={6}
+        onLoad={onMapLoad}
+      >
+        <MarkerF
+          position={selectedLocation}
+          icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
+        />
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default MapComponent;
